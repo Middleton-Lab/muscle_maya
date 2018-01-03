8724da371f360594f4f5f692b8fb940f4dcb91bc
Kevin Middleton <middletonk@missouri.edu>
2018-01-03 16:50:09 -0600

    Reorganize make_mel into function

--- make_mel_standalone.py
+++ make_mel_standalone.py
@@ -79,148 +79,133 @@ pd_version = float(pd.__version__[2:])
 if pd_version <= 17.0:
     raise ImportWarning("pandas version should be >= 0.17.1")
 
-control_file = '/Users/kmm/Google Drive/Work/Research/Alligator Maya/specimens/Control_File.xlsx'
-ctrl = pd.read_excel(control_file)
-
-# base_path = '/Users/kmm/Google Drive/Work/Research/Alligator Maya/specimens/alligator/AL_008'
-# stlfile = 'AL_008.stl'
-# datafile = 'AL_008_joints.xlsx'
-# sheet_name = 'Sheet1'
-# scale_radius = True
-# cylinder_r_max = 8
-# rev_arrows = True
-# rescale_factor = 1
-
 def make_mel(base_path, stlfile, datafile, sheet_name, scale_radius, cylinder_r_max, rev_arrows, rescale_factor):
-    pass
+    file_prefix = stlfile[:-4]
 
+    outfile = os.path.join(base_path, file_prefix + ".mel")
 
-file_prefix = stlfile[:-4]
+    # Read data file
+    M = pd.read_excel(os.path.join(base_path, datafile), sheet_name=sheet_name)
 
-outfile = os.path.join(base_path, file_prefix + ".mel")
+    M = M[M.ID == file_prefix]
 
-# Read data file
-M = pd.read_excel(os.path.join(base_path, datafile), sheet_name=sheet_name)
-
-M = M[M.ID == file_prefix]
-
-# Set up radii for cylinder and cone
-if scale_radius:
-    # Normalize to maximum force value
-    M['cylinder_r'] = M.force / max(M.force)
-    M.cylinder_r *= cylinder_r_max
-else:
-    M['cylinder_r'] = cylinder_r_max / 2
-
-M['cone_r'] = M.cylinder_r * 2
-M['cone_hr'] = 2  # cone_r / 2
-
-# Open mel script outfile for writing
-f = open(outfile, 'w')
-
-# Write header info
-f.write('// File: ' + outfile + '\n')
-f.write('// Generated: ' + time.strftime("%Y/%m/%d %H:%M:%S") + '\n')
-f.write('// Note: the ratio of max to min forces is ')
-f.write(str(round(max(M.force) / min(M.force), 3)))
-f.write('.\n\n')
-
-# Import shader information
-f.write('// Import color shader presets\n')
-f.write('file -import -type "mayaBinary"  -ignoreVersion -ra true ')
-f.write('-mergeNamespacesOnClash false -namespace "Color_Presets" ')
-f.write('-options "v=0;"  -pr "Color_Presets.mb";\n\n')
-
-# Import model. Note need full path to stl.
-f.write('// Import Alligator model\n')
-f.write('file -import -type "STL_ATF"  -ignoreVersion -ra true ')
-f.write('-mergeNamespacesOnClash false -namespace "' + file_prefix + '" ')
-f.write('-pr "' + os.path.abspath(stlfile) + '";\n')
-f.write('rename polySurface1 stl_model;\n')
-f.write('select -r stl_model;\n')
-f.write('hyperShade -assign Color_Presets:Bone;\n')
-f.write('hide stl_model;\n\n')
-
-# Loop through muscles in coordinates file
-for data in M.itertuples():
-    muscle = data.muscle[0] + data.muscle[2:]
-    f.write('// Muscle ' + muscle + ';\n')
-
-    if rev_arrows:
-        insertion_x = data.x_insertion
-        insertion_y = data.y_insertion
-        insertion_z = data.z_insertion
-        origin_x = data.x_origin
-        origin_y = data.y_origin
-        origin_z = data.z_origin
+    # Set up radii for cylinder and cone
+    if scale_radius:
+        # Normalize to maximum force value
+        M['cylinder_r'] = M.force / max(M.force)
+        M.cylinder_r *= cylinder_r_max
     else:
-        # Note reversing origin and insertion from coords file to put the arrows
-        # on the correct end.
-        insertion_x = data.x_origin
-        insertion_y = data.y_origin
-        insertion_z = data.z_origin
-        origin_x = data.x_insertion
-        origin_y = data.y_insertion
-        origin_z = data.z_insertion
+        M['cylinder_r'] = cylinder_r_max / 2
 
-    origin_coords = str(origin_x) + ' ' + str(origin_y) + ' ' + \
-        str(origin_z)
-    insertion_coords = str(insertion_x) + ' ' + str(insertion_y) + ' ' + \
-        str(insertion_z)
+    M['cone_r'] = M.cylinder_r * 2
+    M['cone_hr'] = 2  # cone_r / 2
 
-    # Euler angles
-    origin = array([origin_x, origin_y, origin_z])
-    insertion = array([insertion_x, insertion_y, insertion_z])
-    B = insertion - origin
-    A = array([0., 1., 0.])
-    R = get_euler_angles(A, B)
-    rotations = str(R[0]) + ' ' + str(R[1]) + ' ' + str(R[2])
+    # Open mel script outfile for writing
+    f = open(outfile, 'w')
+
+    # Write header info
+    f.write('// File: ' + outfile + '\n')
+    f.write('// Generated: ' + time.strftime("%Y/%m/%d %H:%M:%S") + '\n')
+    f.write('// Note: the ratio of max to min forces is ')
+    f.write(str(round(max(M.force) / min(M.force), 3)))
+    f.write('.\n\n')
+
+    # Import shader information
+    f.write('// Import color shader presets\n')
+    f.write('file -import -type "mayaBinary"  -ignoreVersion -ra true ')
+    f.write('-mergeNamespacesOnClash false -namespace "Color_Presets" ')
+    f.write('-options "v=0;"  -pr "Color_Presets.mb";\n\n')
+
+    # Import model. Note need full path to stl.
+    f.write('// Import Alligator model\n')
+    f.write('file -import -type "STL_ATF"  -ignoreVersion -ra true ')
+    f.write('-mergeNamespacesOnClash false -namespace "' + file_prefix + '" ')
+    f.write('-pr "' + os.path.abspath(stlfile) + '";\n')
+    f.write('rename polySurface1 stl_model;\n')
+    f.write('select -r stl_model;\n')
+    f.write('hyperShade -assign Color_Presets:Bone;\n')
+    f.write('hide stl_model;\n\n')
+
+    # Loop through muscles in coordinates file
+    for data in M.itertuples():
+        muscle = data.muscle[0] + data.muscle[2:]
+        f.write('// Muscle ' + muscle + ';\n')
+
+        if rev_arrows:
+            insertion_x = data.x_insertion
+            insertion_y = data.y_insertion
+            insertion_z = data.z_insertion
+            origin_x = data.x_origin
+            origin_y = data.y_origin
+            origin_z = data.z_origin
+        else:
+            # Note reversing origin and insertion from coords file to put the arrows
+            # on the correct end.
+            insertion_x = data.x_origin
+            insertion_y = data.y_origin
+            insertion_z = data.z_origin
+            origin_x = data.x_insertion
+            origin_y = data.y_insertion
+            origin_z = data.z_insertion
+
+        origin_coords = str(origin_x) + ' ' + str(origin_y) + ' ' + \
+            str(origin_z)
+        insertion_coords = str(insertion_x) + ' ' + str(insertion_y) + ' ' + \
+            str(insertion_z)
+
+        # Euler angles
+        origin = array([origin_x, origin_y, origin_z])
+        insertion = array([insertion_x, insertion_y, insertion_z])
+        B = insertion - origin
+        A = array([0., 1., 0.])
+        R = get_euler_angles(A, B)
+        rotations = str(R[0]) + ' ' + str(R[1]) + ' ' + str(R[2])
 
 
-    # Create a curve from origin to insertion
-    f.write('curve -n curve1 -d 1 -p ' + origin_coords + ' -p ' +
-        insertion_coords + ' -k 0 -k 1;\n')
+        # Create a curve from origin to insertion
+        f.write('curve -n curve1 -d 1 -p ' + origin_coords + ' -p ' +
+            insertion_coords + ' -k 0 -k 1;\n')
 
-    # Create a circle at the origin with a (0, 1, 0) normal
-    f.write('circle -n circ -ch on -o on -c ' + origin_coords + ' -nrx 0 ')
-    f.write('-nry 1 -nrz 0 -radius ' + str(data.cylinder_r) + ';\n')
+        # Create a circle at the origin with a (0, 1, 0) normal
+        f.write('circle -n circ -ch on -o on -c ' + origin_coords + ' -nrx 0 ')
+        f.write('-nry 1 -nrz 0 -radius ' + str(data.cylinder_r) + ';\n')
 
-    # Apply Euler rotations
-    f.write('rotate -r -pivot ' + origin_coords + ' -xyz ' + rotations +
-        ' circ;\n')
+        # Apply Euler rotations
+        f.write('rotate -r -pivot ' + origin_coords + ' -xyz ' + rotations +
+            ' circ;\n')
 
-    # Extrude cylinder
-    f.write('extrude -n ' + muscle + 'cyl -et 1 -po 0 circ curve1;\n')
+        # Extrude cylinder
+        f.write('extrude -n ' + muscle + 'cyl -et 1 -po 0 circ curve1;\n')
 
-    # Make, rotate, and move cone
-    f.write('cone -n ' + muscle + 'Cone -po 0 -axis 0 1 0 -r ' +
-        str(data.cone_r) + ' -hr ' + str(data.cone_hr) + ';\n')
-    f.write('rotate -r -xyz ' + rotations + ' ' + muscle + 'Cone;\n')
-    f.write('move ' + insertion_coords + ' ' + muscle + 'Cone;\n')
+        # Make, rotate, and move cone
+        f.write('cone -n ' + muscle + 'Cone -po 0 -axis 0 1 0 -r ' +
+            str(data.cone_r) + ' -hr ' + str(data.cone_hr) + ';\n')
+        f.write('rotate -r -xyz ' + rotations + ' ' + muscle + 'Cone;\n')
+        f.write('move ' + insertion_coords + ' ' + muscle + 'Cone;\n')
 
-    # Clean up
-    f.write('select -r curve1;\n')
-    f.write('doDelete;\n')
-    f.write('select -r circ;\n')
-    f.write('doDelete;\n')
+        # Clean up
+        f.write('select -r curve1;\n')
+        f.write('doDelete;\n')
+        f.write('select -r circ;\n')
+        f.write('doDelete;\n')
 
-    # Apply shader
-    shader = 'Color_Presets:' + muscle[1:] + "SG"
-    f.write('select -r ' + muscle + 'Cone' + ' ' + muscle + 'cyl;\n')
-    f.write('hyperShade -assign ' + shader + ';\n')
+        # Apply shader
+        shader = 'Color_Presets:' + muscle[1:] + "SG"
+        f.write('select -r ' + muscle + 'Cone' + ' ' + muscle + 'cyl;\n')
+        f.write('hyperShade -assign ' + shader + ';\n')
 
-    # Reverse surface normals
-    f.write('reverseSurface -ch on -rpo on -d 3 ' + muscle + 'cyl;\n\n')
+        # Reverse surface normals
+        f.write('reverseSurface -ch on -rpo on -d 3 ' + muscle + 'cyl;\n\n')
 
-# Unhide stl_model
-f.write('// Unhide stl_model;\n')
-f.write('showHidden stl_model;\n')
+    # Unhide stl_model
+    f.write('// Unhide stl_model;\n')
+    f.write('showHidden stl_model;\n')
 
-# Group all objects together
-f.write('// Group objects for animation;\n')
-f.write('')
+    # Group all objects together
+    f.write('// Group objects for animation;\n')
+    f.write('')
 
-f.close()
+    f.close()
 
 # defaultAreaLight(1, 1,1,1, 0, 0, 0,0,0, 1, 0);
 # setAttr "areaLight1.rotateX" -90;
@@ -235,3 +220,16 @@ f.close()
 # setAttr "areaLightShape1.intensity" 0.2;
 # setAttr "areaLightShape2.intensity" 0.2;
 #
+
+control_file = '/Users/kmm/Google Drive/Work/Research/Alligator Maya/specimens/Control_File.xlsx'
+ctrl = pd.read_excel(control_file)
+
+
+# base_path = '/Users/kmm/Google Drive/Work/Research/Alligator Maya/specimens/alligator/AL_008'
+# stlfile = 'AL_008.stl'
+# datafile = 'AL_008_joints.xlsx'
+# sheet_name = 'Sheet1'
+# scale_radius = True
+# cylinder_r_max = 8
+# rev_arrows = True
+# rescale_factor = 1
