861f6982df622b013977517232fd98a839cd4ebc
Kevin Middleton <middletonk@missouri.edu>
2018-01-05 17:08:27 -0600

    Use careful option for gifsicle

--- animate_gif.sh
+++ animate_gif.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gifsicle --delay=10 --loop images/*.gif > gifsicle_anim.gif
+gifsicle --careful --delay=10 --loop images/*.gif > gifsicle_anim.gif
 convert -delay 10 -loop 0 images/*.gif convert_anim.gif
